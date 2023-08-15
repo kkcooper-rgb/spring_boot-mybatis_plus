95ea4fd923521bf80e65328ef93611e9b3c4938e
hanwankui <wankui.han@tengwow.com>
2023-08-15 17:43:53 +0800

    初始化

---
+++ src/main/java/com/kk/WaimaiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaimaiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaimaiDemoApplication.class, args);
+    }
+
+}
